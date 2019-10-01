8e6a762d5cf0f60db53f617ee8f56dbeeec23f99
GokulGithub1234 <55776485+GokulGithub1234@users.noreply.github.com>
2019-10-01 11:36:28 +0530

    FSE - Final Check-In

---
+++ src/shared/data-transfer.service.ts
@@ -0,0 +1,10 @@
+import { Injectable } from '@angular/core';
+import { Task } from 'src/app/entities/task';
+
+
+@Injectable()
+export class DataTransferService {
+
+    public task: Task;
+    constructor() { }
+}
